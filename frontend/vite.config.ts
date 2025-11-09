70766191af33560edfaf7ef67678c9647b7ac440
Sciappa <marco.pietraforte@tiscali.it>
2025-11-09 17:13:57 +0100

    DEPLOY: Integrato frontend React Sport + routing Express + build produzione

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -54,7 +54,14 @@
       outDir: 'build',
     },
     server: {
-      port: 3000,
+      port: 5173,
       open: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false
+        }
+      }
     },
   });
